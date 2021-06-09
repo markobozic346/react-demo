4982456705cec14d24f6c957d9564e4ce3296b76
markobozic346 <55995892+markobozic346@users.noreply.github.com>
2021-06-09 08:07:21 +0200

    loading animation

--- src/components/ShowList.tsx
+++ src/components/ShowList.tsx
@@ -1,23 +1,22 @@
-import React, { useEffect, useState } from "react";
-import { showsService } from 'services/showService'
+import React, { useEffect, useState } from 'react';
+import { showsService } from 'services/showService';
+import LoadingAnimation from 'components/Animations/LoadingAnimation';
 const ShowList = () => {
-
-  const [shows, setShows] = useState([])
-  const [loading, setLoading] = useState<true | false>(true)
+  const [shows, setShows] = useState([]);
+  const [loading, setLoading] = useState<true | false>(true);
 
   useEffect(() => {
-    setLoading(true)
-    fetchShows()
-  }, [])
+    setLoading(true);
+    fetchShows();
+  }, []);
 
   const fetchShows = async function () {
     const fetchedShows = await showsService.fetchData();
-    setShows(fetchedShows)
-    setLoading(false)
+    setShows(fetchedShows);
+    setLoading(false);
+  };
 
-  }
-
-  return <div>{loading ? 'loading...' : 'ready'}</div>;
+  return <div>{loading ? <LoadingAnimation /> : 'ready'}</div>;
 };
 
 export default ShowList;
